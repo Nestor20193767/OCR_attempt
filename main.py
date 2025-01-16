d3b6332ee6d13d4f2e70d27632e543c258282da3
Nestor Allende <74310266+Nestor20193767@users.noreply.github.com>
2025-01-15 20:12:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,20 @@ from datetime import datetime
 
 # Crear el diálogo para mostrar el texto
 @st.dialog("Mostrar tu texto")
-def show_text_dialog():
-    user_text = st.text_input("Escribe algo aquí...")
-    
-    # Botón para mostrar el texto ingresado en el diálogo
-    if st.button("Mostrar en el diálogo"):
-        if user_text:
-            st.session_state.user_message = user_text
-            st.rerun()  # Recargar la página para activar el diálogo
-        else:
-            st.write("Por favor, ingresa algo antes de presionar el botón.")
+def show_text_dialog(text):
+    st.write(text)
+    if st.button("Genial"):
+        st.session_state.texto = 'existente'
+        st.rerun
 
-# Verificar si ya se ha guardado el texto en el estado de la sesión
-if "user_message" in st.session_state:
-    st.write(f"Tu mensaje es: {st.session_state.user_message}")
-else:
-    st.write("Ingresa un texto y presiona el botón para mostrarlo en el diálogo.")
+if "texto" not in st.session_state:
+    texto = st.text("Escribe algo...")
+    boton = st.button("Got it")
+    if boton:
+        show_text_dialog(texto)
+    
+
+
 
 
 
