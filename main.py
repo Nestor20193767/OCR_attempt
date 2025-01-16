1fb2423fe64738443ac80509c05f6ccfcdda2a3d
Nestor Allende <74310266+Nestor20193767@users.noreply.github.com>
2025-01-15 20:19:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ def show_text_dialog(text):
 
 texto = st.text_input("Escribe algo...")
 boton = st.button("Got it")
-if boton && dia == 16::
-    show_text_dialog(texto)
+if boton:
+    if dia == 16:
+        show_text_dialog(texto)
     
 
 
